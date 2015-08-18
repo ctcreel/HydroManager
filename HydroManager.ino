3bf19d8289ff5596ab0b4497744fcd0c127214ef
Christopher Creel <ctcreel.business@gmail.com>
2015-08-18 12:22:38 -0400

    Simplifications
    Consolidated the relay classes in to one class to simplify things a bit.
    
    Also pulled all the sitter functionality in to the main file.

--- HydroManager.ino
+++ HydroManager.ino
@@ -1,4 +1,4 @@
-#include "sitter.h"
+// #include "sitter.h"
 #include "DEBUG.h"
 #include "Commands.h"
 #include "Hardware.h"
@@ -10,7 +10,8 @@
 #include "config.h"
 #include "Sensors.h"
 #include "DHT.h"
-#include "fogger.h"
+#include "Logger.h"
+#include "relayArray.h"
 
 // Third party libraries
 #include <Wire.h>
@@ -23,21 +24,18 @@
 #include <EEPROM.h>
 #include <EEPROMAnything.h>
 
-sitter *s;
-manageROM *c;
-logger *loggerDevice;
 RTC_DS1307 RTC;
 unsigned int heightMeasurements[MEASUREMENTS];
 unsigned int heightMeasures;
 
 generatorDeviceID gID;
-fogger f(FOGGER_PIN);
-
-eventStream *e;
-
-class environment {
-
-};
+manageROM c;
+relayArray array(LIGHT_PIN,FAN_PIN,PUMP_PIN,FOGGER_PIN);
+sensorAnalog waterLevel(WATER_LEVEL_PIN,WATER_LEVEL_POWER,WATER_LEVEL_SENSOR_POWER_UP,MAX_WATER_LEVEL,MIN_WATER_LEVEL);
+sensorAnalog moisture(MOISTURE_PIN, MOISTURE_POWER, MOISTURE_SENSOR_POWER_UP,MAX_MOISTURE,MIN_MOISTURE);
+sensorAnalog soilTemp(TEMP_PIN, TEMP_POWER, TEMP_SENSOR_POWER_UP,MAX_TEMP,MIN_TEMP);
+logger loggerDevice(SD_ONE, SD_TWO, SD_THREE, SD_FOUR);
+eventStream e(&Serial3,&gID);
 
 time_t syncProvider()     //this does the same thing as RTC_DS1307::get()
 {
@@ -45,94 +43,83 @@ time_t syncProvider()     //this does the same thing as RTC_DS1307::get()
 }
 
 void setup () {
+  // c.reset();
   DEBUG_BEGIN(BAUD_RATE);
   Wire.begin();
-  loggerDevice = new logger(SD_ONE, SD_TWO, SD_THREE, SD_FOUR);
-  c = new manageROM;
-  //c->reset();
-  setUpClock();
-  setUpSitter();
-  setUpRadio();
-  resetFlowerCheck();
-  // Make sure we collect the height regularly
-  Alarm.timerRepeat(c->getHeightInterval(), getHeight);
-  Alarm.timerRepeat(60, getHumidityAndTemp);
+  Serial.begin(BAUD_RATE);
+
+  /* Set up clock */ 
+  RTC.begin();
+  setSyncProvider(syncProvider);     //reference our syncProvider function instead of RTC_DS1307::get()
+
+  /* Set up events */
+  Serial3.begin(BAUD_RATE);
+  
+  new eventOutgoing(&e, getMoisture,SET_MOISTURE,GET_MOISTURE);
+  new eventOutgoing(&e, getWaterLevel,SET_WATER_LEVEL,GET_WATER_LEVEL);
+  new eventOutgoing(&e, getSoilTemp,SET_SOIL_TEMP,GET_SOIL_TEMP);
+  new eventOutgoing(&e, getTime,SET_TIME,GET_TIME);
+  new eventOutgoing(&e, lightIsOn,SET_LIGHT_ON,GET_LIGHT_ON);
+  new eventOutgoing(&e, fanIsOn,SET_FAN_ON,GET_FAN_ON);
+  new eventOutgoing(&e, pumpIsOn,SET_PUMP_ON,GET_PUMP_ON);
+  
+  /* Set up configuration settings */
+  
+  new eventOutgoing(&e, c.getDesiredMoisture, SET_DESIRED_MOISTURE,GET_DESIRED_MOISTURE);
+  new eventIncoming(&e, c.setDesiredHumidity, SET_DESIRED_MOISTURE);
+  new eventOutgoing(&e, c.getLightStartTime, SET_START_TIME, GET_START_TIME);
+  new eventIncoming(&e, c.setLightStartTime, SET_START_TIME);
+  new eventOutgoing(&e, c.getLightOnTime, SET_TIME_ON, GET_TIME_ON);
+  new eventIncoming(&e, c.setLightOnTime, SET_TIME_ON);
+  new eventOutgoing(&e, c.getCoolDownTime, SET_COOL_DOWN, GET_COOL_DOWN);
+  new eventIncoming(&e, c.setCoolDownTime, SET_COOL_DOWN);
+  new eventOutgoing(&e, c.getPumpOnTime, SET_PUMP_ON, GET_PUMP_ON);
+  new eventIncoming(&e, c.setPumpOnTime, SET_PUMP_ON);
+  
+  /* Set up remote devices */
+  
+  new eventIncoming(&e, logDistance, SET_DISTANCE);
+  new eventIncoming(&e, logHeight, SET_HEIGHT);
+  new eventIncoming(&e, logHeightAlert, SET_DISTANCE_ALARM);
+  new eventIncoming(&e, logAirTemp, SET_AIR_TEMP);
+  new eventIncoming(&e, setHumidity, SET_HUMIDITY);
+  
+  new eventOutgoing(&e, getGrowMode, SET_GROW_MODE, GET_GROW_MODE);
+
+  DEBUG("Light time on - " + String(c.getLightOnTime()));
+  
+  /* Set up alarms */
+  Alarm.timerRepeat(c.getHeightInterval(), getHeight);
+  Alarm.timerRepeat(60, getHumidity);
+  Alarm.timerRepeat(60, getTemp);
+  Alarm.timerRepeat(60, checkMoisture);
   Alarm.alarmRepeat(23,00,00,flowerCheck);
+  Alarm.alarmRepeat(24,00,00,dailySetup);
+
+  /* Let's get started */
+  dailySetup();
+  resetFlowerCheck();
  }
 
 void loop()
 {  
-  e->check(0);
+  e.check(0);
 }
 
-void getHeight(void) {
-  e->createEvent("",GET_DISTANCE);
-}
-
-void setUpSitter(void) {
-
-    DEBUG("Light on time during setup is " + String(c->getLightOnTime()));
-
-    TEMP_PIN, TEMP_POWER, TEMP_SENSOR_POWER_UP,
-    
-    s = new sitter(
-      10, // maximum low water counts before notification
-      c,
-      new light(LIGHT_PIN),
-      new fan(FAN_PIN),
-      new pump(PUMP_PIN),
-      new sensorAnalog(WATER_LEVEL_PIN,WATER_LEVEL_POWER,WATER_LEVEL_SENSOR_POWER_UP,MAX_WATER_LEVEL,MIN_WATER_LEVEL),
-      new sensorAnalog(MOISTURE_PIN, MOISTURE_POWER, MOISTURE_SENSOR_POWER_UP,MAX_MOISTURE,MIN_MOISTURE),
-      new sensorAnalog(TEMP_PIN, TEMP_POWER, TEMP_SENSOR_POWER_UP,MAX_TEMP,MIN_TEMP),
-      loggerDevice
-   );
-}
-
-void setUpRadio(void) {
-
-   Serial3.begin(BAUD_RATE);
-   e = new eventStream(&Serial3,&gID);
-
-   new eventOutgoing(e, s->getMoisture,SET_MOISTURE,GET_MOISTURE);
-   new eventOutgoing(e, s->getWaterLevel,SET_WATER_LEVEL,GET_WATER_LEVEL);
-   new eventOutgoing(e, s->getSoilTemp,SET_SOIL_TEMP,GET_SOIL_TEMP);
-   new eventOutgoing(e, s->getTime,SET_TIME,GET_TIME);
-   new eventOutgoing(e, s->lightIsOn,SET_LIGHT_ON,GET_LIGHT_ON);
-   new eventOutgoing(e, s->fanIsOn,SET_FAN_ON,GET_FAN_ON);
-   new eventOutgoing(e, s->pumpIsOn,SET_PUMP_ON,GET_PUMP_ON);
-   
-   /* Set up configuration settings */
-  
-   new eventOutgoing(e, c->getDesiredMoisture, SET_DESIRED_MOISTURE,GET_DESIRED_MOISTURE);
-   new eventIncoming(e, c->setDesiredHumidity, SET_DESIRED_MOISTURE);
-   new eventOutgoing(e, c->getLightStartTime, SET_START_TIME, GET_START_TIME);
-   new eventIncoming(e, c->setLightStartTime, SET_START_TIME);
-   new eventOutgoing(e, c->getLightOnTime, SET_TIME_ON, GET_TIME_ON);
-   new eventIncoming(e, c->setLightOnTime, SET_TIME_ON);
-   new eventOutgoing(e, c->getCoolDownTime, SET_COOL_DOWN, GET_COOL_DOWN);
-   new eventIncoming(e, c->setCoolDownTime, SET_COOL_DOWN);
-   new eventOutgoing(e, c->getPumpOnTime, SET_PUMP_ON, GET_PUMP_ON);
-   new eventIncoming(e, c->setPumpOnTime, SET_PUMP_ON);
-   
-   /* Set up remote devices */
-   
-   new eventIncoming(e, logDistance, SET_DISTANCE);
-   new eventIncoming(e, logHeight, SET_HEIGHT);
-   new eventIncoming(e, logHeightAlert, SET_DISTANCE_ALARM);
-   new eventIncoming(e, logAirTemp, SET_AIR_TEMP);
-   new eventIncoming(e, setHumidity, SET_HUMIDITY);
-   
-   new eventOutgoing(e, getGrowMode, SET_GROW_MODE, GET_GROW_MODE);
-}
-
-void setUpClock(void) {
-  RTC.begin();
-  setSyncProvider(syncProvider);     //reference our syncProvider function instead of RTC_DS1307::get()
-
-//  for debugging purposes
-//  setTime(17,15,00,1,1,2015);
-//  RTC.adjust(now());
-//  DEBUG("Current time is - " + String(hour()) + ":" + String(minute()) + " on " + String(month()) + "-" + String(day()) + "-" + String(year()));
+void dailySetup(void) {
+  const unsigned long current = (second() + (((unsigned long) minute())*60) + (((unsigned long) hour())*3600));
+  const unsigned long startTime = c.getLightStartTime();
+  const unsigned long endTime = startTime + c.getLightOnTime();
+  if(current >= 0 && current < startTime) {
+    // Looks like a normal cycle so schedule wake-up alarm
+    Alarm.timerOnce(startTime, dailySetup);
+  } else if(current >= startTime && current < endTime) {
+    // Looks like we woke up after the wake-up alarm but before the day is over
+    // Turn the light on for the remainder of the day.
+    unsigned long timeOn = endTime - current;
+    array.turnOnOne(timeOn);
+    array.turnOnTwo(timeOn + c.getCoolDownTime());
+  }
 }
 
 void resetFlowerCheck(void) {
@@ -147,13 +134,13 @@ void flowerCheck(void) {
     if(dailyMeasurements >= MIN_MEASUREMENTS) { // If we don't have enough so data don't do anything
       for(unsigned int i = 0; i < dailyMeasurements; i++) averageHeight += heightMeasurements[i];
       averageHeight /= dailyMeasurements;
-      if(averageHeight >= flowerAtHeight && c->getLightOnTime() != LIGHT_ON_TIME_FLOWER) {
-        c->setLightOnTime(LIGHT_ON_TIME_FLOWER);
-        c->setLightStartTime(LIGHT_START_TIME_FLOWER);
+      if(averageHeight >= flowerAtHeight && c.getLightOnTime() != LIGHT_ON_TIME_FLOWER) {
+        c.setLightOnTime(LIGHT_ON_TIME_FLOWER);
+        c.setLightStartTime(LIGHT_START_TIME_FLOWER);
         startFlowering();
-      } else if(averageHeight < flowerAtHeight && c->getLightOnTime() != LIGHT_ON_TIME_VEG) {
-        c->setLightOnTime(LIGHT_ON_TIME_VEG);
-        c->setLightStartTime(LIGHT_START_TIME_VEG);
+      } else if(averageHeight < flowerAtHeight && c.getLightOnTime() != LIGHT_ON_TIME_VEG) {
+        c.setLightOnTime(LIGHT_ON_TIME_VEG);
+        c.setLightStartTime(LIGHT_START_TIME_VEG);
       }
       resetFlowerCheck();
     }
@@ -164,112 +151,131 @@ void startFlowering(void) {
   if(start < 12) {
     start++;
     Alarm.timerOnce(300, startFlowering);
-    e->createEvent("",START_FLOWERING);
+    e.createEvent("",START_FLOWERING);
   } else {
     logStartFlowering();
   }
 }
 
-void logStartFlowering(void) {
-    char message[100];
-    sprintf(message, "%02d:%02d,%02d-%02d-%04d,%d",
-      hour(),
-      minute(),
-      day(),
-      month(),
-      year());
-  loggerDevice->logMessage("FLOWERING.TXT",message);
+void setHumidity(const unsigned long h) {
+  logValue("HUMIDITY.TXT",h);
+  
+  if((getGrowMode()==0 && h < 65) || (getGrowMode()==1 && h < 50)) {
+    if(!array.isOnFour()) {
+      array.turnOnFour(300);
+    }
+  }
 }
 
+void checkMoisture(void) {
+  unsigned long moisture = getMoisture();
+  unsigned long soilTemp = getSoilTemp();
+  if(getMoisture() < c.getDesiredMoisture()) {
+    array.turnOnThree(c.getPumpOnTime());
+  }
+}
+
+/* Status Functions */
+
 const unsigned long getGrowMode(void) {
-  if(c->getLightOnTime() == LIGHT_ON_TIME_FLOWER) {
+  if(c.getLightOnTime() == LIGHT_ON_TIME_FLOWER) {
     return 1;
-  } else if(c->getLightOnTime() == LIGHT_ON_TIME_VEG) {
+  } else if(c.getLightOnTime() == LIGHT_ON_TIME_VEG) {
     return 0;
   } else {
     return 3;
   }
 }
 
+void getHeight(void) {
+  e.createEvent("",GET_DISTANCE);
+}
+
+void getHumidity(void) {
+  e.createEvent("",GET_HUMIDITY);
+}
+
+void getTemp(void) {
+  e.createEvent("",GET_AIR_TEMP);
+}
+
+const unsigned long sdIsPresent(void) {
+  return loggerDevice.sdIsPresent() ? 1 : 0;
+}
+
+const unsigned long fanIsOn(void) {
+  return array.isOnTwo() ? 1 : 0;
+}
+
+const unsigned long pumpIsOn(void) {
+  return array.isOnThree() ? 1 : 0;
+}
+
+const unsigned long lightIsOn(void) {
+  return array.isOnOne() ? 1 : 0;
+}
+
+const unsigned long getSoilTemp(void) {
+  return soilTemp.getReading();
+}
+
+const unsigned long getMoisture(void) {
+  return moisture.getReading();
+}
+
+const unsigned long getWaterLevel(void) {
+  return waterLevel.getReading();
+}
+
+const unsigned long getTime(void) {
+  return now();
+}
+
+/* Logging Functions */
+
+void logValue(char *file, unsigned long h) {
+    char message[100];
+    sprintf(message, "%02d:%02d,%02d-%02d-%04d,%d",
+      hour(),
+      minute(),
+      day(),
+      month(),
+      year(),
+      h);
+  loggerDevice.logMessage(file,message);
+  DEBUG(String(file) + " logged value " + String(h));
+}
+
+void logStartFlowering(void) {
+  logValue("FLOWERING.TXT",1);
+}
+
 void logHeight(unsigned long h) {
-  char message[100];
-  sprintf(message, "%02d:%02d,%02d-%02d-%04d,%d",
-    hour(),
-    minute(),
-    day(),
-    month(),
-    year(),
-    h);
+  logValue("HEIGHT.TXT",h);
   heightMeasurements[heightMeasures % MEASUREMENTS] = h;
   heightMeasures++;
-  loggerDevice->logMessage("HEIGHT.TXT",message);
-  DEBUG("Plant height is - " + String(h));
 }
 
 void logDistance(unsigned long h) {
-    char message[100];
-    sprintf(message, "%02d:%02d,%02d-%02d-%04d,%d",
-      hour(),
-      minute(),
-      day(),
-      month(),
-      year(),
-      h);
-  loggerDevice->logMessage("DISTANCE.TXT",message);
-  DEBUG("Distance from light is - " + String(h));
+  logValue("DISTANCE.TXT",h);
 }
 
 void logHeightAlert(unsigned long h) {
-    char message[100];
-    sprintf(message, "%02d:%02d,%02d-%02d-%04d,%d",
-      hour(),
-      minute(),
-      day(),
-       month(),
-      year(),
-      h);
-  loggerDevice->logMessage("ALARM_H.TXT",message);
-  Serial.print("Distance alert - ");
-  Serial.println(message);
+  logValue("HEIGHT_ALARM.TXT",h);
 }
-
 
 void logAirTemp(unsigned long h) {
-    char message[100];
-    sprintf(message, "%02d:%02d,%02d-%02d-%04d,%d",
-      hour(),
-      minute(),
-      day(),
-      month(),
-      year(),
-      h);
-  loggerDevice->logMessage("AIRTEMP.TXT",message);
-  DEBUG("Air temp is - " + String(h));
+  logValue("AIRTEMP.TXT",h);
 }
 
-void setHumidity(const unsigned long h) {
-    char message[100];
-    sprintf(message, "%02d:%02d,%02d-%02d-%04d,%d",
-      hour(),
-      minute(),
-      day(),
-       month(),
-      year(),
-      h);
-  loggerDevice->logMessage("HUMIDITY.TXT",message);
-  Serial.print("Humidity is - ");
-  Serial.println(message);
-  
-  if((getGrowMode()==0 && h < 65) || (getGrowMode()==1 && h < 50)) {
-    if(!f.isOn()) {
-      f.turnOn(600);
-    }
-  }
+void logMoisture(unsigned long h) {
+  logValue("MOISTURE.TXT",h);
 }
 
-void getHumidityAndTemp(void) {
-  Serial.println("Get humidity!");
-  e->createEvent("",GET_HUMIDITY);
-  e->createEvent("",GET_AIR_TEMP);
+void logSoilTemp(unsigned long h) {
+  logValue("SOIL_TEMP.TXT",h);
 }
 
+void logWaterLevel(unsigned long h) {
+  logValue("WATER_LEVEL.TXT",h);
+}
---
+++ relayArray.cpp
@@ -0,0 +1,87 @@
+#include "relayArray.h"
+#include "DEBUG.h"
+#include <TimeAlarms.h>
+
+relay *relayArray::relayOne;
+relay *relayArray::relayTwo;
+relay *relayArray::relayThree;
+relay *relayArray::relayFour;
+
+relayArray::relayArray(byte pinOne,byte pinTwo,byte pinThree,byte pinFour) {
+    relayOne = new relay(pinOne);
+    relayTwo = new relay(pinTwo);
+    relayThree = new relay(pinThree);
+    relayFour = new relay(pinFour);
+}
+
+relayArray::~relayArray(void) {
+  delete relayOne;
+  delete relayTwo;
+  delete relayThree;
+  delete relayFour;
+}
+
+void relayArray::turnOffOne(void) {
+    relayOne->turnOff();
+    DEBUG("Turning relayOne off");
+}
+
+void relayArray::turnOffTwo(void) {
+    relayTwo->turnOff();
+    DEBUG("Turning relayTwo off");
+}
+
+void relayArray::turnOffThree(void) {
+    relayThree->turnOff();
+    DEBUG("Turning relayThree off");
+}
+
+void relayArray::turnOffFour(void) {
+    relayFour->turnOff();
+    DEBUG("Turning relayFour off");
+}
+
+
+void relayArray::turnOnOne(unsigned long timeOn) {
+    if(relayOne->turnOn()){
+        Alarm.timerOnce(timeOn, turnOffOne);
+        DEBUG("relayOne turned on and schedule to turn off in " + String(timeOn) + " seconds");
+    }
+}
+
+void relayArray::turnOnTwo(unsigned long timeOn) {
+    if(relayTwo->turnOn()){
+        Alarm.timerOnce(timeOn, turnOffTwo);
+        DEBUG("relayTwo turned on and schedule to turn off in " + String(timeOn) + " seconds");
+    }
+}
+
+void relayArray::turnOnThree(unsigned long timeOn) {
+    if(relayThree->turnOn()){
+        Alarm.timerOnce(timeOn, turnOffThree);
+        DEBUG("relayThree turned on and schedule to turn off in " + String(timeOn) + " seconds");
+    }
+}
+
+void relayArray::turnOnFour(unsigned long timeOn) {
+    if(relayFour->turnOn()){
+        Alarm.timerOnce(timeOn, turnOffFour);
+        DEBUG("relayFour turned on and schedule to turn off in " + String(timeOn) + " seconds");
+    }
+}
+
+const boolean relayArray::isOnOne(void) {
+  return relayOne->isOn();
+};
+
+const boolean relayArray::isOnTwo(void) {
+  return relayTwo->isOn();
+};
+
+const boolean relayArray::isOnThree(void) {
+  return relayThree->isOn();
+};
+
+const boolean relayArray::isOnFour(void) {
+  return relayFour->isOn();
+};
